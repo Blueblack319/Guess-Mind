80ff0897c5f1ff3f33840e0d4c34b056797ea615
Jae Hoon Yang <didwogns789@naver.com>
2020-08-29 20:50:40 +0900

    Set up socket.io

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 ## Cloning Catch-Mind with Vanilla JS, Node JS, SocketIO and Gulp
 
+## To Do:
+
 - [ ] Set up
+  - [x] Server
+  - [x] Pug
+  - [x] Static
+  - [x] Eslint
+  - [ ] SocketIO
 - [ ] Socket IO
 - [ ] Gulp
 - [ ] Login
--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
 import express from "express";
+import path from "path";
+import socketIO from "socket.io";
 
 const PORT = 4000;
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+const handleHome = (req, res) => {
+  res.render("home");
+};
+
+app.get("/", handleHome);
+
 const handleListening = () => {
   console.log(`✅ Listening on: http://localhost:${PORT}`);
 };
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+  console.log("Somebody connect!");
+});
