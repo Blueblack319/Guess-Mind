20b2f2aa83dd043242b87e7c0155495527aab422
Jae Hoon Yang <didwogns789@naver.com>
2020-08-30 10:32:46 +0900

    Improve login flow

---
+++ assets/js/login.js
@@ -0,0 +1,33 @@
+const loginForm = document.getElementById("js-loginForm");
+const body = document.querySelector("body");
+
+const NICKNAME = "nickname";
+const LOGGED_IN = "loggedIn";
+const LOGGED_OUT = "loggedOut";
+
+const nickname = localStorage.getItem(NICKNAME);
+
+const login = (nickname) => {
+  // eslint-disable-next-line no-undef
+  const socket = io();
+  socket.emit("setNickname", { nickname });
+};
+
+if (nickname === null) {
+  body.className = LOGGED_OUT;
+} else {
+  body.className = LOGGED_IN;
+  login(nickname);
+}
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const input = loginForm.querySelector("input");
+  const { value } = input;
+  input.value = "";
+  localStorage.setItem(NICKNAME, value);
+  body.className = LOGGED_IN;
+  login(value);
+};
+
+loginForm.addEventListener("submit", handleFormSubmit);
