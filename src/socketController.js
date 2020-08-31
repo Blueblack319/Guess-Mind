6a90794e07b106af48cc4bf1304bcf4d37468eca
Jae Hoon Yang <didwogns789@naver.com>
2020-08-31 11:16:00 +0900

    Make func broadcast

--- src/socketController.js
+++ src/socketController.js
@@ -1,27 +1,29 @@
 import events from "./events";
 
 const socketController = (socket) => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
-    socket.broadcast.emit(events.newUser, { nickname });
+    broadcast(events.newUser, { nickname });
   });
   socket.on(events.disconnect, () => {
-    socket.broadcast.emit(events.disconnected, { nickname: socket.nickname });
+    broadcast.emit(events.disconnected, { nickname: socket.nickname });
   });
   socket.on(events.sendMsg, ({ message }) => {
-    socket.broadcast.emit(events.newMsg, {
+    broadcast.emit(events.newMsg, {
       message,
       nickname: socket.nickname,
     });
   });
   socket.on(events.beginPath, ({ x, y }) =>
-    socket.broadcast.emit(events.beganPath, { x, y })
+    broadcast.emit(events.beganPath, { x, y })
   );
   socket.on(events.beginStroke, ({ x, y, color }) =>
-    socket.broadcast.emit(events.beganStroke, { x, y, color })
+    broadcast.emit(events.beganStroke, { x, y, color })
   );
   socket.on(events.fill, ({ color }) =>
-    socket.broadcast.emit(events.filled, { color })
+    broadcast.emit(events.filled, { color })
   );
 };
 
